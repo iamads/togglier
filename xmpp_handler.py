8c8e94b80e3bb1f7311080d75062435e0da29361
abhijeet <abhijeet3192@gmail.com>
2015-03-28 18:26:30 +0530

    add_controller is now working , it takes username and password as parameters

--- xmpp_handler.py
+++ xmpp_handler.py
@@ -1,17 +1,23 @@
 import requests 
 from xml.sax.saxutils import escape
-def add_controller(name,password):
-	xml = """
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<user>
-    <username>{}</username>
-    <password>{}<password>
-</user>""".format(escape(name), escape(password))
-	headers = {'Content-Type': 'application/xml'}
-	r = requests.post(url='http://192.168.200.115:9090/plugins/userService/users',
-	headers=headers,
-	data=xml,	
-	auth=('admin','admin'))
-	print r
 
-add_controller("test@example.com","test")
+def add_controller(name,password):
+	xml = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<user>
+	<username>{}</username>
+	<password>{}</password>
+</user>""".format(escape(name), escape(password))
+
+	headers = {'Content-Type': 'application/xml','Authorization': 'Basic YWRtaW46MTIzNDU='}
+	r = requests.post(
+    	url='http://192.168.200.105:9090/plugins/userService/users',
+    	data=xml, headers=headers, 
+    	auth=('admin', 'admin')
+	)
+	print r 
+
+
+
+
+add_controller("bitch","bitch")
+
