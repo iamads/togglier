d2997003be8216555ebb6e34eaab73bd61b6bd4e
abhijeet <abhijeet3192@gmail.com>
2015-03-30 15:28:38 +0530

    Added initiate.sh for controller which would be the on boot script it will handle xmpp and other scripts which are required. It takes xmpp username and password as args

---
+++ controller/initiate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+"""This script is the one which would be run on boot
+It starts the controller_xmpp.py which handles the xmpp communication
+and then forwards the data to controller.py."""
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+bash "${SCRIPTPATH}/controller.py" &
+bash "${SCRIPTPATH}/controller_xmpp.py" -j $1 -p $2 
