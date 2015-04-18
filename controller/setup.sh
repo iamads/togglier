a3b1e797db9c9334fe6b73aae675e20c5e0f7daf
Anuj Gupta <anuj.hrk@gmail.com>
2015-04-18 16:47:48 +0530

    created one time setup user enters UserName and Password and then after running userId and password stored and would be persistent after reboot

--- controller/setup.sh
+++ controller/setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+read -p "Enter UserId : " UserId
+read -p "Enter Password : " pass
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+crontab -l > mynewcron
+echo "@reboot ${SCRIPTPATH}/initiate.sh ${UserId} ${pass}" >> mynewcron
+crontab mynewcron
+rm mynewcron
+
 apt-get update
 apt-get install -y python-zmq python-pip
 pip install sleekxmpp
