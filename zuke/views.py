18b52f64b9022302d4c3c21c45c5ebf7aa701ff0
abhijeet <abhijeet3192@gmail.com>
2015-04-18 14:27:01 +0530

    Changing templates

--- zuke/views.py
+++ zuke/views.py
@@ -1,7 +1,8 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hi")
+    context_dict = {'boldmessage':"I am from context"}
+    return render(request, '/rango/index.html', context_dict)
