534f25b2171042b2346027ebf7867c4d55f8cbd0
Anuj Gupta <anuj.hrk@gmail.com>
2015-04-18 19:16:42 +0530

    keep in mind to use gtalk for testing

--- controller/initiate.sh
+++ controller/initiate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-"""This script is the one which would be run on boot
-It starts the controller_xmpp.py which handles the xmpp communication
-and then forwards the data to controller.py."""
+#"""This script is the one which would be run on boot
+#It starts the controller_xmpp.py which handles the xmpp communication
+#and then forwards the data to controller.py."""
 
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 python "${SCRIPTPATH}/controller.py" &
