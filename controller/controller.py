dea9b0bd6e3601c657941e0c528876b09dc9e261
abhijeet <abhijeet3192@gmail.com>
2015-04-22 19:14:58 +0530

    Changed controller.py to add more a dictionary of devices

--- controller/controller.py
+++ controller/controller.py
@@ -1,15 +1,17 @@
 import zmq
 import RPi.GPIO as GPIO
 
+gpio_dict = {"device_1": 7, "device_2": 12, "device_3": 16, "device_4": 5}
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+
+# GPIO.setup(7, GPIO.OUT)
+for gpio in gpio_dict.items():
+    GPIO.setup(gpio, GPIO.OUT)
 
 context = zmq.Context()
 
 socket = context.socket(zmq.PAIR)
 socket.connect("tcp://127.0.0.1:5696")
-
-gpio_dict = {"device_1": 7, "device_2": 12, "device_3": 15, "device_4": 18}
 
 while True:
     command = str(socket.recv())
